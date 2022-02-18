43dffbff1d48db2f95c7f79f7b08b0768062f9cc
Brian161 <99992293+Brian161@users.noreply.github.com>
2022-02-18 16:25:09 -0500

    Update PurchaseFrame.java

--- PurchaseFrame.java
+++ PurchaseFrame.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ramirezbrianproject3;
+package brianproject3;
 
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author brianramirez
+ * @author brian
  */
 public class PurchaseFrame extends javax.swing.JFrame {
     
