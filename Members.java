1d2b9533f362ea77ed337b96732020c027281d00
Brian161 <99992293+Brian161@users.noreply.github.com>
2022-02-18 16:23:23 -0500

    Update Members.java

--- Members.java
+++ Members.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ramirezbrianproject3;
+package brianproject3;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author brianramirez
+ * @author brian
  */
 public class Members {
 
